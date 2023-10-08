cfbd5ee1b4224238a19dc2d572b6d09c8c68400b
Valeriusdev <kakao757@gmail.com>
2023-10-08 05:01:51 -0700

    delete react.svg and vite.svg

--- recipes/src/App.jsx
+++ recipes/src/App.jsx
@@ -1,16 +1,12 @@
-
-
 function App() {
- 
 
   return (
     <>
       <div>
-      <h1 className="text-3xl font-bold underline">
-      Happy coding!
-    </h1>
-      </div>
-      
+        <h1 className="text-3xl font-bold underline">
+          Happy coding!
+        </h1>
+      </div>      
     </>
   )
 }
