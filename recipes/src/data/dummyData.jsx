8ece7806436b68b502db53fcfe65fc9fdf3ba794
Valeriusdev <kakao757@gmail.com>
2023-10-09 04:31:13 -0700

    create dummyData.jsx and Recipes.jsx

---
+++ recipes/src/data/dummyData.jsx
@@ -0,0 +1,40 @@
+const dummyRecipes = [
+  {
+    id: 1,
+    title: "Spaghetti Carbonara",
+    steps: [
+      "Boil water in a large pot.",
+      "Add spaghetti and cook until al dente.",
+      "In a separate pan, fry pancetta until crispy.",
+      "Whisk eggs, grated Pecorino Romano cheese, and black pepper in a bowl.",
+      "Drain cooked spaghetti and mix with the egg mixture.",
+      "Add pancetta and stir until well combined. Serve hot.",
+    ],
+  },
+  {
+    id: 2,
+    title: "Chicken Alfredo",
+    steps: [
+      "Season chicken breasts with salt and pepper.",
+      "Cook chicken in a skillet until fully cooked and no longer pink.",
+      "In a separate saucepan, melt butter and add heavy cream.",
+      "Stir in grated Parmesan cheese and garlic powder until the sauce thickens.",
+      "Slice cooked chicken and add it to the sauce.",
+      "Serve the chicken Alfredo sauce over cooked fettuccine pasta.",
+    ],
+  },
+  {
+    id: 3,
+    title: "Vegetable Stir-Fry",
+    steps: [
+      "Chop your favorite vegetables (e.g., bell peppers, broccoli, carrots).",
+      "Heat vegetable oil in a wok or large skillet.",
+      "Add minced garlic and ginger, then stir-fry the vegetables until tender-crisp.",
+      "In a small bowl, mix soy sauce, oyster sauce, and a bit of sugar.",
+      "Pour the sauce over the stir-fried vegetables and toss to coat.",
+      "Serve the vegetable stir-fry over steamed rice.",
+    ],
+  },
+];
+
+export default dummyRecipes;
