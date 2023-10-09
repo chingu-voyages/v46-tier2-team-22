23e51851a95705f43d7f3c03d2af6d04d5bff7ee
Valeriusdev <kakao757@gmail.com>
2023-10-09 03:04:24 -0700

    create components folder and Header.jsx

--- recipes/src/App.jsx
+++ recipes/src/App.jsx
@@ -1,14 +1,15 @@
-function App() {
+import React from "react";
+import { Header } from "./components/Header";
 
+function App() {
   return (
     <>
+      <Header />
       <div>
-        <h1 className="text-3xl font-bold underline">
-          Happy coding!
-        </h1>
-      </div>      
+        <h1 className="text-3xl font-bold underline">Happy coding!</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
