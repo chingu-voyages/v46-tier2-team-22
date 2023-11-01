77285b4122ae4569ecb35c95afaf678c378ceeb8
Valeriusdev <kakao757@gmail.com>
2023-11-01 09:54:03 -0700

    add Footer component

--- recipes/src/App.jsx
+++ recipes/src/App.jsx
@@ -1,10 +1,12 @@
 // import { Header } from "./components/Header";
 import { MainPage } from "./components/MainPage";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <MainPage />
+      <Footer />
     </>
   );
 }
