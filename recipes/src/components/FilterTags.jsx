264f897783d4efe2cec9b17832850d6cc71f5646
louis.code <louis.code88@gmail.com>
2023-11-07 13:08:40 -0700

    fix: breakup filter tags vs types

--- recipes/src/components/FilterTags.jsx
+++ recipes/src/components/FilterTags.jsx
@@ -4,6 +4,7 @@ import fetchData from "../utils/fetchData";
 
 function FilterTags({ setFilterTagValue, setIsChangedState }) {
   const [tagsList, setTagsList] = useState([]);
+  const [selectedType, setSelectedType] = useState("");
   const filterTypes = Array.from(new Set(tagsList.map(x => x.root_tag_type)));
 
   useEffect(() => {
@@ -16,31 +17,44 @@ function FilterTags({ setFilterTagValue, setIsChangedState }) {
     setIsChangedState(true);
   }
 
+  function handleFilterTypes(e) {
+    setSelectedType(e.target.value);
+  }
+
   return (
-    <div className="flex p-3 justify-start">
-      <label className="pr-2">Apply filters: </label>
+    <div className="hidden sm:flex py-3 justify-start text-sm">
+      <label className="pr-2 px-2">Search filters: </label>
       <select
-        name="filters"
-        id="filters"
-        className="bg-Pewter"
-        onChange={handleFilter}
+        name="filterTypes"
+        id="filterTypes"
+        className="bg-Pewter px-2 w-40 overflow-clip"
+        onChange={handleFilterTypes}
       >
-        <option value="" disabled>
-          -- Apply tag filters here --
-        </option>
+        <option value="">-- Select filter type --</option>
         {filterTypes.map(type => (
-          <optgroup label={type} key={type}>
-            {tagsList.map(
-              tag =>
-                tag.root_tag_type === type && (
-                  <option value={tag.name} key={tag.id}>
-                    {tag.display_name}
-                  </option>
-                )
-            )}
-          </optgroup>
+          <option key={type} value={type}>
+            {type.slice(0, 1).toUpperCase() + type.slice(1)}
+          </option>
         ))}
       </select>
+      {selectedType ? (
+        <select
+          name="filters"
+          id="filters"
+          className="bg-Pewter px-2 w-32 md:w-40 lg:w-48 overflow-clip"
+          onChange={handleFilter}
+        >
+          <option value="">-- Select filter tag --</option>
+          {tagsList.map(
+            tag =>
+              tag.root_tag_type === selectedType && (
+                <option key={tag.id} value={tag.name}>
+                  {tag.display_name}
+                </option>
+              )
+          )}
+        </select>
+      ) : null}
     </div>
   );
 }
